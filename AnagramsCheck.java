f7a10434146fd9af8d77ef3f068bec4f637412d3
Bala Krishna <prince.balakrishna@gmail.com>
2017-12-10 12:47:30 +0530

    Update AnagramsCheck.java

--- AnagramsCheck.java
+++ AnagramsCheck.java
@@ -20,7 +20,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class AnagramsCheck
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
